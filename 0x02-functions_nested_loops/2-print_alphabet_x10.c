84dd28ce83550e84854beef9b3afe447c75289c9
Omolara Ajanaku <ajanakuomolara53@gmail.com>
2023-04-25 17:27:30 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int y;
+	int count = 0;
+	char letter;
 
-	for (y = 1; i <= 10; i++)
+	while (count++ <= 9)
 	{
-		for (y = 97; y <= 122; y++)
-		{
-			_putchar(y);
-		}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
