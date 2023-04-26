d6ac7b95f9c14d45e5f38f1526b0cd9d1f6be271
Omolara Ajanaku <ajanakuomolara53@gmail.com>
2023-04-25 20:28:47 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_times_table - Prints thetarting with 0
+ * @n: number of the time table
+ */
+void print_times_table(int n)
+{
+	int x;
+	int y;
+	int z;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = x * y;
+
+				if (z <= 99)
+					_putchar(' ');
+				if (z <= 9)
+					_putchar(' ');
+
+				if (z >= 100)
+				{
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10)) % 10 + '0');
+				}
+				else if (z <= 99 && z >= 10)
+				{
+					_putchar((z / 10) + '0');
+				}
+				_putchar((z % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
