a2e2a072c8682fb8a07a8e2739321125c1588823
Omolara Ajanaku <ajanakuomolara53@gmail.com>
2023-04-25 17:18:11 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase.
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int y;
+
+	for (y = i; i <= 10; i++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
