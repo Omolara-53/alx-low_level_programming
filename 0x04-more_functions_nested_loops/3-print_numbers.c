a33040d450fc3284f61843c37d68cefdd1a04a28
Omolara Ajanaku <ajanakuomolara53@gmail.com>
2023-04-27 15:12:20 -0700

    Tas 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Prints 0 to 9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+}
