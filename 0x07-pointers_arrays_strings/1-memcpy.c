0a13984db7b5df3eae8415d13c11f2d916e0f045
Omolara Ajanaku <ajanakuomolara53@gmail.com>
2023-05-09 16:21:47 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,8 +9,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-for (i = 0; i < n; i++)
-dest[i] = src[i];
-return (dest);
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
 }
