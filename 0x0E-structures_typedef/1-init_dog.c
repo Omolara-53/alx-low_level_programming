f38e59c78d7a47316d0c58dd90fa637d27ec504c
Omolara Ajanaku <ajanakuomolara53@gmail.com>
2023-05-23 22:36:52 -0700

    Task 0

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,13 +8,6 @@
  * @age: age of the dog to initialize
  * @owner: owner of dog to initialize
  */
-/**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
- */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
